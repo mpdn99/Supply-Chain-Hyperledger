39c14df5f0117ff944e7c4aaba95f284c42821e2
mpdn99 <mpdn99@gmail.com>
2022-12-15 11:27:53 +0000

    update shell script

--- host3/host3up.sh
+++ host3/host3up.sh
@@ -1,1 +1,10 @@
+echo "====================Initialization===================="
 docker-compose -f host3.yaml up -d
+
+cd ..
+
+echo "====================Join order to channel===================="
+osnadmin channel join --channelID supplychain --config-block channel-artifacts/genesis.block -o localhost:9053 --ca-file ./organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt --client-cert ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.crt --client-key ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.key
+
+echo "====================Join peer to channel====================="
+docker exec cli peer channel join -b channel-artifacts/genesis.block
