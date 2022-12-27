9532cb42cb8fa0a5f8f046b4f564bece879b8da0
mpdn99 <mpdn99@gmail.com>
2022-12-27 08:03:27 +0000

    generate chaincode file

--- host1/host1up.sh
+++ host1/host1up.sh
@@ -6,5 +6,8 @@ cd ..
 echo "====================Join order to channel===================="
 osnadmin channel join --channelID supplychain --config-block channel-artifacts/genesis.block -o localhost:7053 --ca-file ./organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt --client-cert ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.crt --client-key ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.key
 
+osnadmin channel join --channelID supplychain --config-block channel-artifacts/genesis.block -o localhost:11053 --ca-file ./organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt --client-cert ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.crt --client-key ./organizations/ordererOrganizations/example.com/users/Admin@example.com/tls/client.key
+
+
 echo "====================Join peer to channel====================="
 docker exec cli peer channel join -b channel-artifacts/genesis.block
